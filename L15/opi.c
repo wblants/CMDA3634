fc5e64e4930ea1e13926cb2a1585bf1279e186b2
wblants <wblanton@vt.edu>
2018-03-13 16:36:23 -0400

    Adding updated opi.c file

--- L15/opi.c
+++ L15/opi.c
@@ -1,48 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
+
+#include <omp.h>
 
 int main(int argc, char **argv) {
-
+  printf("+");
   //seed random number generator
   // Q2b: get the number of threads to run with from agrv and 
   // add OpenMP API code to set number of threads here
-  int Nthreads = 1;
+  int Nthreads = atoi(argv[1]);
   
+  printf("+");
+  omp_set_num_threads(Nthreads);
+
+  printf("+");
   struct drand48_data *drandData; 
   drandData = (struct drand48_data*) malloc(Nthreads*sizeof(struct drand48_data));
 
   // Q2c: add an OpenMP parallel region here, wherein each thread initializes 
   //      one entry in drandData using srand48_r and seed based on thread number
-  long int seed = 0;
-  srand48_r(seed, drandData+0);
+  
+  printf("+");
+  #pragma omp parallel 
+  {
+    long int seed = omp_get_thread_num();
+    srand48_r(seed, drandData+0);
+  }
 
   long long int Ntrials = 10000000;
-
 
   //need running tallies
   long long int Ntotal=0;
   long long int Ncircle=0;
 
-  for (long long int n=0; n<Ntrials; n++) {
-    double rand1;
-    double rand2;
+  #pragma omp parallel reduction(+:Ncircle)
+  {
+      double startTime = omp_get_wtime();
 
-    //gererate two random numbers (use the thread id to offset drandData)
-    drand48_r(drandData+0, &rand1);
-    drand48_r(drandData+0, &rand2);
-    
-    double x = -1 + 2*rand1; //shift to [-1,1]
-    double y = -1 + 2*rand2;
+      #pragma omp for 
+      for(long long int n=0; n<Ntrials; n++) 
+      {
+        double rand1;
+        double rand2;
 
-    //check if its in the circle
-    if (sqrt(x*x+y*y)<=1) Ncircle++;
-    Ntotal++;
+        //gererate two random numbers (use the thread id to offset drandData)
+        drand48_r(drandData+0, &rand1);
+        drand48_r(drandData+0, &rand2);
+        
+        double x = -1 + 2*rand1; //shift to [-1,1]
+        double y = -1 + 2*rand2;
 
-    if (n%100 ==0) {
-      double pi = 4.0*Ncircle/ (double) (n);
-      printf("Our estimate of pi is %g \n", pi);
-    }
+        //check if its in the circle
+        if (sqrt(x*x+y*y)<=1) Ncircle++;
+        Ntotal++;
+
+        if (n%100 ==0) {
+          double pi = 4.0*Ncircle/ (double) (n);
+          printf("Our estimate of pi is %g \n", pi);
+        }
+      }
+
+      double runTime = omp_get_wtime();
+      printf("The parallel run time was %f seconds.\n", runTime - startTime);
   }
 
   double pi = 4.0*Ncircle/ (double) (Ntotal);
