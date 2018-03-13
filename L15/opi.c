81149b2a0c1eb1e3f40947a6f9d3eafafbabe0b7
wblants <wblanton@vt.edu>
2018-03-13 16:39:00 -0400

    Removed unecessary print statements.

--- L15/opi.c
+++ L15/opi.c
@@ -6,23 +6,20 @@
 #include <omp.h>
 
 int main(int argc, char **argv) {
-  printf("+");
+
   //seed random number generator
   // Q2b: get the number of threads to run with from agrv and 
   // add OpenMP API code to set number of threads here
   int Nthreads = atoi(argv[1]);
   
-  printf("+");
   omp_set_num_threads(Nthreads);
 
-  printf("+");
   struct drand48_data *drandData; 
   drandData = (struct drand48_data*) malloc(Nthreads*sizeof(struct drand48_data));
 
   // Q2c: add an OpenMP parallel region here, wherein each thread initializes 
   //      one entry in drandData using srand48_r and seed based on thread number
   
-  printf("+");
   #pragma omp parallel 
   {
     long int seed = omp_get_thread_num();
